0a2b60b4294b48e2000310c1e582b16b2a75accc
waleedbinalim <waleedbinalim@gmail.com>
2024-05-18 22:04:31 +0500

    chore: minor styling change, used inline, might resort to css or tailwind later

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -6,9 +6,27 @@ const CounterComp: React.FC = () => {
 
   return (
     <>
-      <button onClick={() => dispatch(increment(1))}>
-        count is {state.count}
-      </button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "20px",
+        }}
+      >
+        <h1 style={{ marginBottom: "8px" }}>Counter component</h1>
+        <button
+          onClick={() => dispatch(increment(1))}
+          style={{
+            borderRadius: "20px",
+            width: "20%",
+            padding: "8px",
+            cursor: "pointer",
+          }}
+        >
+          Count = {state.count}
+        </button>
+      </div>
     </>
   );
 };
