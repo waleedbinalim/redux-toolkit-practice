0bcb3ce89dc5151ff4cdeb31a3e436bc90f856b8
waleedbinalim <waleedbinalim@gmail.com>
2024-05-31 13:27:40 +0500

    chore: added vitest and ran base test

---
+++ src/tests/tasks-slice.test.ts
@@ -0,0 +1,19 @@
+import { expect, test, describe } from "vitest";
+import { addTask, createTask, tasksReducer } from "@/slice";
+
+describe("tasksSlice", () => {
+  const initialState = {
+    entities: [
+      createTask({ title: "Test Task 1" }),
+      createTask({ title: "Test Task 2" }),
+    ],
+  };
+
+  test(`Should add task when ${addTask}`, () => {
+    const task = createTask({ title: "Test Task 3" });
+    const action = addTask(task);
+    const newState = tasksReducer(initialState, action);
+
+    expect(newState.entities).toEqual([task, ...initialState.entities]);
+  });
+});
