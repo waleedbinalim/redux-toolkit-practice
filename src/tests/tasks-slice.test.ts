8c9dec11a8dbca68064af3de764b3444c6357817
waleedbinalim <waleedbinalim@gmail.com>
2024-06-01 17:55:36 +0500

    chore: more test cases added

--- src/tests/tasks-slice.test.ts
+++ src/tests/tasks-slice.test.ts
@@ -1,5 +1,11 @@
 import { expect, test, describe } from "vitest";
-import { addTask, createTask, tasksReducer } from "@/slice";
+import {
+  addTask,
+  createTask,
+  tasksReducer,
+  removeTask,
+  assignTaskToUser,
+} from "@/slice";
 import { generateTasks } from "@/mock-server/mock-data-generator";
 
 describe("tasksSlice", () => {
@@ -14,5 +20,26 @@ describe("tasksSlice", () => {
     const newState = tasksReducer(initialState, action);
 
     expect(newState.entities).toEqual([task, ...initialState.entities]);
+  });
+
+  test(`Should remove task when ${addTask}`, () => {
+    const task = initialState.entities[0];
+    const action = removeTask(task.id);
+    const newState = tasksReducer(initialState, action);
+
+    expect(newState).not.toContainEqual(task);
+  });
+
+  test(`Should assign task to user when ${assignTaskToUser}`, () => {
+    const userId = "1";
+    const task = initialState.entities[0];
+    const action = assignTaskToUser({ taskId: task.id, userId });
+    const newState = tasksReducer(initialState, action);
+
+    const foundTask = newState.entities.find(
+      (stateTask) => stateTask.id === task.id
+    );
+
+    expect(foundTask).toHaveProperty("user", userId);
   });
 });
